7c26ebcb2e8335ebc414749b7a0094dea6987ed8
KlassCode <=klassenedmond7@gmail.com>
2022-06-06 11:46:12 -0400

    work in progress

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-from pickle import TRUE
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -10,5 +9,5 @@ DEBUG = True
 # Connect to the database
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:nbaklass0597@localhost:5432/flyuur'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:000@localhost:5432/flyuur'
 SQLALCHEMY_TRACK_MODIFICATIONS=True
